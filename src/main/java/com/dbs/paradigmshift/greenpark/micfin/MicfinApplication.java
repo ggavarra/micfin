5e3b221bebe0edf88ce3bddbb76e735a81c53af6
ggavarra <gayathri1.g@gmail.com>
2019-07-15 08:02:27 +0800

    Initial Commit

---
+++ src/main/java/com/dbs/paradigmshift/greenpark/micfin/MicfinApplication.java
@@ -0,0 +1,14 @@
+package com.dbs.paradigmshift.greenpark.micfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicfinApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/dbs/paradigmshift/greenpark/micfin/MicfinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dbs.paradigmshift.greenpark.micfin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicfinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
