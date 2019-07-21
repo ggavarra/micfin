3b10a8415195d5502db3ae63da421dd84253a5f6
ggavarra <gayathri1.g@gmail.com>
2019-07-21 17:37:46 +0800

    adding swagger

--- src/main/java/com/dbs/paradigmshift/greenpark/micfin/MicfinApplication.java
+++ src/main/java/com/dbs/paradigmshift/greenpark/micfin/MicfinApplication.java
@@ -2,8 +2,11 @@ package com.dbs.paradigmshift.greenpark.micfin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+//Swagger UI will be available at http://localhost:<portnumber>/swagger-ui.html
 public class MicfinApplication {
 
 	public static void main(String[] args) {
